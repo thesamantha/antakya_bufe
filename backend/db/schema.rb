8c0bd113044f4c0e7a1b181eb0f0f63e177081d1
Samantha Webber <samanthatitewebber@gmail.om>
2019-01-20 22:22:54 +0100

    Implemented basic login, appearance of news box only if user logged in.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190102221704) do
+ActiveRecord::Schema.define(version: 20190120164530) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "type"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190102221704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
   create_table "orders", force: :cascade do |t|
     t.float    "total"
     t.boolean  "finished"
@@ -25,4 +26,13 @@ ActiveRecord::Schema.define(version: 20190102221704) do
     t.datetime "updated_at",    null: false
     t.string   "customer_name"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
