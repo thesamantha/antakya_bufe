fb95a559d1d7b14e7dbbd06a6453c5138c89c1b5
Samantha Webber <samanthatitewebber@gmail.om>
2019-01-24 21:37:55 +0100

    Correct CSS issues in home page by adding files to config/initializers/assets.rb, removing application.html.erb layout from home controller

--- backend/config/initializers/assets.rb
+++ backend/config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( antakyahome_mobile.css )
+Rails.application.config.assets.precompile += %w( antakyahome_tablet.css )
+Rails.application.config.assets.precompile += %w( antakyahome_full_screen.css )
+Rails.application.config.assets.precompile += %w( bejelentkezo-ablak.js )
