9b1c6c6b3a36e29acaa97ba7782e001aa5768bc9
Konyhai Ottó <konyhaiotto@gmail.com>
2019-02-12 21:51:52 +0100

    trying out different effects with CSS

--- frontend/automaticSlideshow.js
+++ frontend/automaticSlideshow.js
@@ -11,5 +11,5 @@ function showSlides() {
  slideIndex++;
  if (slideIndex > slides.length) {slideIndex = 1}
    slides[slideIndex-1].style.display = "block";
-   setTimeout(showSlides, 4000);
+   setTimeout(showSlides, 5500);
 }
