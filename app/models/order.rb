b9c28722c410e8536602df8849fbce93a5df7bf8
Samantha Webber <samanthatitewebber@gmail.om>
2019-02-18 19:05:14 +0100

    Write step logic in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,43 @@ class Order < ApplicationRecord
                                 reject_if: lambda { |f| f[:quantity].blank? }
 
   validates :foods, presence: true
+  validates :customer_name, presence: true
+  validates :address, presence: true
+  validates :tele, presence: true
+
+
+  # logic for multiple-step order
+
+  attr_writer :current_step # setter-method for :current_step
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w[food delivery].freeze
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
+  def all_valid?
+    steps.all? do |step|
+      self.current_step = step
+      valid?
+    end
+  end
 end
